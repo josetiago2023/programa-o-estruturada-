641a6b666c9ae7d7711f9fff2153f45e11588be8
josetiago2023 <141453973+josetiago2023@users.noreply.github.com>
2026-04-29 20:01:22 +0000

    1

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,78 @@
-contador = 1
+# contador = 1
 
-while contador <= 5:
-    print(f"Contador: {contador}")
-    contador += 1 g
+# while contador <= 5:
+#     print(f"Contador: {contador}")
+#     contador += 1 g
+
+
+#     # const idade = 18
+
+#     # pode_dirigir = idade > 18 ? "sim " "nao":
+#     # javascript
+
+
+#     #SOlucao 1
+#     if numero > 0:
+#         print("positivo")
+#     elif numero < 0:
+#         print("Negativo")
+
+#     #solucaop2
+#     if numero > 0:
+#         print(positivo)
+#     if numero < 0:
+#         print("Negativo")
+#     if numero == 0:
+#         print("Zero")
+    
+#     #solucao3
+#     resultado = "Positivo" if numero > 0 else r = "Negativo" if numero < 0 
+#     else "Zero"
+#     print(resultado)
+
+#     match numero:
+#         case 0:
+#             print("zero")
+#         case x if x > 0:
+#             print("positivo")
+#         case x if  x < 0 :
+#             print("Negativo")
+#         case _:
+#             print("fim")
+
+
+
+# n = int(input())
+# if n % 2 == 0:
+#     print("par")
+# else:
+#     print("impar")
+
+# n1 = int(input("Digite um numero:"))
+# n2 = int(input())
+# soma = n1 + n2
+# subt = n1 - n2
+# mult = n1 * n2
+# divi = n1 / n2
+# print(soma , subt, mult, divi)
+
+# n1 = int(input())
+# n2 = int(input())
+# n3 = int(input())
+# if n1 > n2 and n1 > n3:
+#     print(n1)
+# elif n2 > n1 and n2 > n3:
+#     print(n2)
+# elif n3 > n1 and n3 > n2:
+#     print(n3)
+# else:
+#     print("é igual")
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+if (n1, n2, n3, n4) <= 12:
+    print("é criança")
+else:
+    print("adulto")
